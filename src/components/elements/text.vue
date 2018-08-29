268c94fc1d8b742c20145cad254a32eba15cbd23
bingqing <958105054@qq.com>
2018-08-29 18:20:54 +0800

    txt组件

---
+++ src/components/elements/text.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="text" :txtColor="txtColor" :isBlock="isBlock" :class="{'txt-code': isCode, 'txt-block': isBlock}" :style="{background: txtColor}">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: 'txt',
+    props: {
+        isCode: {
+            type: Boolean,
+            default: false
+        },
+        txtColor: {
+            type: String,
+            default: '#fff'
+        },
+        isBlock: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+<style scoped>
+.text{
+    display: inline-block;
+    padding: 0 0.05rem;
+    font-size: 14px;
+    color: #333;
+}
+.txt-code{
+    background-color: #fbfaf8!important;
+    padding: 0 0.05rem;
+    border: 1px solid #d6d5d3;
+    border-radius: 3px;
+}
+.txt-block{
+    display: block;
+    margin: 0.05rem 0;
+}
+</style>
+
+
--- src/components/svgCircleSlider.vue
+++ src/components/svgCircleSlider.vue
@@ -1,24 +1,35 @@
 <template>
     <div class="svgCircleSlider">
-        <svg class="svg">
-            <circle cx="144" cy="144" r="140" stroke="#f5f5f5"
-            stroke-width="8" fill="none"/>
-            <circle cx="144" cy="144" r="140" stroke="#c9c0d3"
-            stroke-width="8" fill="none" stroke-dasharray="0 900"
-            transform="matrix(0,-1,1,0,0,288)" class="progressSvg"
-            id="svgId"/>
-            <text class="percent" x="50%" y="50%" text-anchor="middle"
-            dominant-baseline="middle" fill="#c9c0d3" id="percentId">0%</text>
-        </svg>
+        <div class="svgBox">
+            <svg class="svg">
+                <circle cx="144" cy="144" r="140" stroke="#f5f5f5"
+                stroke-width="8" fill="none"/>
+                <circle cx="144" cy="144" r="140" stroke="#c9c0d3"
+                stroke-width="8" fill="none" stroke-dasharray="0 900"
+                transform="matrix(0,-1,1,0,0,288)" class="progressSvg"
+                id="svgId"/>
+                <text class="percent" x="50%" y="50%" text-anchor="middle"
+                dominant-baseline="middle" fill="#c9c0d3" id="percentId">0%</text>
+            </svg>
+        </div>
+       
+        <txt txtColor="#c9c0d3">ddddd</txt>
+        <txt>jsjfljldmlsn;l</txt>
+        <txt is-code is-block>jsjflj</txt>
+
     </div>
 </template>
 
 <script>
+import txt from './../components/elements/text.vue'
+
 export default {
     name: 'svgCircleSlider',
+    components: {
+        txt
+    },
     data() {
         return {
-
         }
     },
     mounted() {
@@ -46,6 +57,9 @@ export default {
 <style scoped>
 .svgCircleSlider{
     padding-top: 1rem;
+    
+}
+.svgBox{
     transform: scale(0.6);
     text-align: center;
 }
@@ -55,6 +69,9 @@ export default {
 .svg{
     width: 288px;
     height: 288px;
+     /* ios手机坐标异常 */
+    -webkit-transform: rotate(-0.05deg); 
+    transform: rotate(-0,05deg);
 }
 .progressSvg{
     stroke-dasharray: 0 900;
