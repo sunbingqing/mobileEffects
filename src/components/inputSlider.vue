3f4014ef39ae4f9b3d888692ae1f5f95c9dab2b8
bingqing <958105054@qq.com>
2018-08-30 10:35:00 +0800

    input滑动条注释

--- src/components/inputSlider.vue
+++ src/components/inputSlider.vue
@@ -5,12 +5,28 @@
             :style="{backgroundSize: point + '% 100%'}" @input="change">
             <span class="sliderTip" :style="{left: point * 0.028 + 'rem'}">{{point}}</span>
         </div>
+        <div class="txtBox">
+            <txt is-block txtColor="#cddae6">input[type=range]</txt>
+            <txt is-block>1# 改写滑块样式：
+                <txt is-code>input[type=range]::-webkit-slider-thumb {<br>&nbsp;&nbsp;-webkit-appearance: none;/*后面跟想要的样式*/<br>}</txt>
+            </txt>
+            <txt is-block>2# 改写轨道样式：
+                <txt is-code>input[type=range] {<br>&nbsp;&nbsp;-webkit-appearance: none;<br>&nbsp;&nbsp;background: -webkit-linear-gradient(left, #8595a5, #8595a5) no-repeat #e1e1e1;
+    <br>&nbsp;&nbsp;background-size: <txt txtColor="#cddae6">50</txt>% 100%;<br>}</txt>
+            </txt>
+            <txt><txt txt-color="#cddae6">background-size</txt>的第一个参数，即为<txt txt-color="#cddae6">input</txt>的<txt txt-color="#cddae6">value</txt>值</txt>
+        </div>
     </div>
 </template>
 
 <script>
+import txt from './../components/elements/text.vue'
+
 export default {
     name: 'inputSlider',
+    components: {
+        txt
+    },
     data() {
         return {
             point: 50
@@ -56,6 +72,11 @@ export default {
     height: 0.2rem;
     background: #fff;
     border: 2px solid #739ac0;
+}
+
+.txtBox{
+    padding-top: 1rem;
+    text-align: left;
 }
 
 </style>
